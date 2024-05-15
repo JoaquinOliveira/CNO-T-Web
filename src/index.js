77715b0d0b67e72610c76d7baca6ec5b7c6671ae
Joaquin Santiago Oliveira <104986417+fr0st1987@users.noreply.github.com>
2024-05-15 16:45:37 +0200

    .env added

--- src/index.js
+++ src/index.js
@@ -4,6 +4,8 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+// Resto del código que utiliza las variables de entorno
+// ...
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
